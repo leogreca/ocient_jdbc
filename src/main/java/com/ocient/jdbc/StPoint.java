e30b11e751ff8f9c84db48bae6829a4244ad4ae8
Leo <lgreca@ocient.com>
2021-04-22 00:21:35 +0000

    the point is empty if any of the coordinates are infinity

--- src/main/java/com/ocient/jdbc/StPoint.java
+++ src/main/java/com/ocient/jdbc/StPoint.java
@@ -34,7 +34,7 @@ public class StPoint
 	@Override
 	public String toString()
 	{
-		if (Double.isInfinite(lat) && Double.isInfinite(lon))
+		if (Double.isInfinite(lat) || Double.isInfinite(lon))
 		{
 			return "POINT EMPTY";
 		}
